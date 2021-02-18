213ef82742e7b5ee00e50462ff2dfb0cca885db7
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-02-18 13:18:45 -0300

    0.3

--- src/main/java/com/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/springboot/web/app/controllers/IndexController.java
@@ -3,14 +3,15 @@ package com.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"/index", "/"})
+    @GetMapping({"/index", "/", ""})
     public String index(Model model) {
         model.addAttribute("titulo", "Hola Spring Framework!!");
         return "index";
     }
-
 }
