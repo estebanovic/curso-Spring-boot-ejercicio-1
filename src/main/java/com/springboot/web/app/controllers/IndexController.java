51874536c5a493f3db01926ced338d4b498ee65a
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-02-19 11:26:06 -0300

    0.4

--- src/main/java/com/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.springboot.web.app.controllers;
 
+import com.springboot.web.app.models.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +16,24 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("titulo", "Hola Spring Framework!!");
         return "index";
+    }
+    
+    @RequestMapping("/perfil")
+    public String perfil(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Esteban");
+        usuario.setApellido("Obilinovic");
+        usuario.setEmail("esteban@gmail.com");
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("titulo", "Perfil de ".concat(usuario.getNombre()));
+        return "perfil";
+    }
+    
+    @RequestMapping("/listar")
+    public String listar(Model model){
+        List<Usuario> usuarios = new ArrayList<>();
+        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("usuarios", usuarios);
+        return "listar";
     }
 }
