fbd7a24452e94eb46e0775f350ab130a4cd7b7ca
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-02-22 13:24:13 -0300

    0.6

--- src/main/java/com/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,14 @@ public class EjemploParamsController {
     
     @GetMapping("/mix-params")
     public String param(@RequestParam String saludo,@RequestParam Integer numero, Model model){
+        model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el numero es  '" + numero +"'");
+        return "params/ver";
+    }
+    
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model){
+        String saludo = request.getParameter("saludo");
+        Integer numero = Integer.parseInt(request.getParameter("numero"));
         model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' y el numero es  '" + numero +"'");
         return "params/ver";
     }
